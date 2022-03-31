8f34849a4a912143a2aed790dc9b2d8503f2ebc0
gamedez <78411694+gamedez@users.noreply.github.com>
2022-04-01 00:08:14 +0300

    Add files via upload

---
+++ leason-1.py
@@ -0,0 +1,21 @@
+# 1 задание
+print("Glo Academy")
+print()
+print("Glo Academy")
+
+# задание 1.1
+
+print("Glo Academy", "Glo Academy", sep="\n \n")
+
+# задание 2
+
+name = input("Как тебя зовут?")
+print("Привет,", name)
+
+# 3
+
+name1 = input("Как вас зовут?")
+name2 = input("Как ваша фамилия?")
+name3 = input("Как ваше отчество?")
+
+print(name2, name1, name3)
